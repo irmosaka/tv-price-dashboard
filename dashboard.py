4821dc18af9256bbb4cee6ebb22b2f4b0b87289e
Mohammad Sadeghi Kaji <61044991+irmosaka@users.noreply.github.com>
2026-02-07 12:38:14 +0330

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -4,7 +4,7 @@ from bs4 import BeautifulSoup
 import pandas as pd
 
 def scrape_tv_prices():
-    url = 'https://www.digikala.com/search/category-tv/'  # عوض کن با URL واقعی
+    url = 'https://www.digikala.com/search/category-tv2/'  
     headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'}
     response = requests.get(url, headers=headers)
     soup = BeautifulSoup(response.text, 'html.parser')
